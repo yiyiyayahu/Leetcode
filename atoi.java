65002518c0f0d2249fba39e2f3020e936ffb1e4d
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-09-29 22:01:26 -0400

    Update atoi.java

--- atoi.java
+++ atoi.java
@@ -1,28 +1,42 @@
-   public int atoi(String str) {
+       public int atoi(String str) {
         
         str = str.trim();
         if(str == null) return 0;
         if(str.length() == 0) return 0;
         if(!isValid(str.charAt(0))) return 0;
         
-        int num = 0;
-        
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == '+' || str.charAt(i) == '-') {
-                continue;
-            } else if(isNumbers(str.charAt(i))) {
+        long num = 0;
+        int sign = 1;
+        int i = 0;
+        if(str.charAt(0) == '-') {
+            sign = -1;
+            i = 1;
+        }
+        if(str.charAt(0) == '+') {
+            i = 1;
+        }
+        for(; i < str.length(); i++) {
+            
+            if(isNumbers(str.charAt(i))) {
+                num *= 10;                 //put num * 10 in the front
                 num += str.charAt(i) - '0';
-                num *= 10;
+                
             } else {
                 break;
             }
         }
-        num /= 10;
-        
-        if(str.charAt(0) == '-') {
-            num = 0 - num;
+        num = num * sign;
+        /*
+        if(sign == 1 && num < 0) {
+            return Integer.MAX_VALUE;
+        } else if(sign == -1 && num > 0) {
+            return Integer.MIN_VALUE;
         }
         return num;
+        */
+        if(num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        else if(num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else return (int)num;
     }
     public boolean isValid(char c) {
         return (c <= '9' && c >= '0') || c == '+' || c == '-';
