6c7eab3254d5ca4ce3d1aa390abb3f286b84f534
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-10 22:44:31 -0400

    Create sorted list to BST.java

---
+++ sorted list to BST.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        int n = 0;
+        ListNode curr = head;
+        while(curr != null) {
+            n ++;
+            curr = curr.next;
+        }
+        return sortedListToBSTHelper(head, 0, n-1);
+    }
+    public TreeNode sortedListToBSTHelper(ListNode head, int start, int end) {
+        if(start > end) return null;
+        
+        int mid = (start + end) / 2;
+        ListNode curr = head;
+        for(int i = start; i < mid; i++) {
+            curr = curr.next;
+        }
+        TreeNode root = new TreeNode(curr.val);
+        TreeNode left = sortedListToBSTHelper(head, start, mid - 1);
+        root.left = left;
+        root.right = sortedListToBSTHelper(curr.next, mid + 1, end);
+        return root;
+    }
+}
