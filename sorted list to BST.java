6d27f90b10f4c7027eddc6e53338c4bf0a5cc0e0
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-19 18:33:47 -0400

    Update sorted list to BST.java

--- sorted list to BST.java
+++ sorted list to BST.java
@@ -40,3 +40,31 @@ public class Solution {
         return root;
     }
 }
+
+//O(n)
+public class Solution {
+    public int i = 0;
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        ListNode curr = head;
+        int n = 0;
+       
+        while(curr != null) {
+            curr = curr.next;
+            n ++;
+        }
+        return BSTHelper(new ListNode[]{head}, 0, n-1);
+    }
+    public TreeNode BSTHelper(ListNode[] head, int start, int end) {
+        if(start > end) return null;
+        int mid = start + (end - start)/2;
+        TreeNode left = BSTHelper(head, start, mid - 1);
+        TreeNode n = new TreeNode(head[0].val);
+        n.left = left;
+        head[0] = head[0].next; //head[0] is a ListNode, so head[0].next will return the next ListNode
+        //head = head.next is wrong. Because we need head to be changed after one iteration, but we first pass in head,
+        //after executing the left treenode, the head should be changed, but here it is still the original head.
+        n.right = BSTHelper(head, mid + 1, end);
+        return n;
+    }
+}
