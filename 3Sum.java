ee05e4431e96b312b5fb431501bfd70a632bb084
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-20 17:40:36 -0400

    Update 3Sum.java

--- 3Sum.java
+++ 3Sum.java
@@ -1,39 +1,33 @@
-//not a very good solution, first sort, and then use two pointers, like two sum, but also use hash set to avoid duplicates
+//To avoid duplicates, if I find the next element to search is the same as this round, skip it.
 public class Solution {
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-        HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
-        if(num == null || num.length < 3) return ret;
         Arrays.sort(num);
-        
-        for(int i = 0; i < num.length-2; i++) {
-        	
-            int target = 0 - num[i];
-            int start = i + 1;
-            int end = num.length - 1;
-            
-            while(start < end) {
-                ArrayList<Integer> list = new ArrayList<Integer>();
-            	int sum = 0;
-                sum = num[start] + num[end];
-                if(sum > target) {
-                    end --;
-                } else if(sum < target) {
-                    start ++;
+        for(int i = 0; i < num.length - 2; i ++) {
+            if(i >=1 && num[i] == num[i-1]) continue; 
+            int first = i + 1;
+            int last = num.length - 1;
+            while(first < last) {
+                int sum = num[first] + num[last] + num[i];
+                if(sum < 0) {
+                    first ++;
+                } else if(sum > 0) {
+                    last --;
                 } else {
-                	list.add(num[i]);
-                	list.add(num[start]);
-                	list.add(num[end]);
-                    set.add(list);
-                    start ++;
+                    ArrayList<Integer> list = new ArrayList<Integer>();
+                    list.add(num[i]);
+                    list.add(num[first]);
+                    list.add(num[last]);
+                    ret.add(list);
+
+                    while(first < last && num[first] == num[first+1]) first ++;
+                    while(first < last && num[last] == num[last-1]) last --;
+                    first ++;
+                    last --; 
                 }
             }
 
         }
-        for(ArrayList<Integer> l : set) {
-            ret.add(l);
-        }
         return ret;
     }
-    
 }
