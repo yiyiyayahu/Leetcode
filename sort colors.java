0b409d2cd6cacf3735c42821b2b8a6b3e4362a02
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-20 21:47:47 -0400

    Update sort colors.java

--- sort colors.java
+++ sort colors.java
@@ -19,3 +19,30 @@ public class Solution {
         }
     }
 }
+
+
+public class Solution {
+    public void sortColors(int[] A) {
+        int index0 = 0;
+        int index2 = A.length - 1;
+        int i = 0;
+        while(i <= index2) {
+            if(A[i] == 0 && i > index0) {
+                swap(A, i, index0);
+                index0 ++;
+                continue;
+            }
+            if(A[i] == 2) {
+                swap(A, i, index2);
+                index2 --;
+                continue;
+            }
+            i++;
+        }
+    }
+    public void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    } 
+}
