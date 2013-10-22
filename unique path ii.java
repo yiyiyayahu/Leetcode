cd62f097ee9e33c33f3cf7b14ef5ef6d83238618
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-22 13:42:46 -0400

    Create unique path ii.java

---
+++ unique path ii.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] path = new int[m][n];
+        
+        if(m < 1 || n < 1) return 0;
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+
+        path[m-1][n-1] = 1;
+        for(int i = m-1; i >= 0; i--) {
+            if(obstacleGrid[i][n-1] == 0) {
+                path[i][n-1] = 1;
+            } else {
+                path[i][n-1] = 0;
+                break;
+            }
+        }
+        for(int j = n-1; j >= 0; j--) {
+            if(obstacleGrid[m-1][j] == 0) {
+                path[m-1][j] = 1;
+            } else {
+                path[m-1][j] = 0;
+                break;
+            }
+        }
+        if(m > 1 && n > 1) {
+            for(int i = m-2; i >= 0; i--) {
+                for(int j = n-2; j >= 0; j--) {
+                    if(obstacleGrid[i][j] == 0) {
+                        path[i][j] = path[i+1][j] + path[i][j+1];
+                    } else {
+                        path[i][j] = 0;
+                    }
+                }
+            }
+        }
+        return path[0][0];
+    }
+}
