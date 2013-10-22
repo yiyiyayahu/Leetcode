877755fe027419b43f4df80026814cfe34094ae4
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-22 14:06:42 -0400

    Update unique path.java

--- unique path.java
+++ unique path.java
@@ -39,3 +39,17 @@ public class Solution {
         }
         return path[0][0];
     }
+
+//better solution
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] path = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) path[i][j]=1;
+                else path[i][j] = (i>0?path[i-1][j]:0) + (j>0?path[i][j-1]:0);
+            }
+        }
+        return path[m-1][n-1];
+    }
+}
