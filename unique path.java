458524ac74092726f2332c75be775478bcd48a98
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-22 12:20:27 -0400

    Update unique path.java

--- unique path.java
+++ unique path.java
@@ -18,3 +18,24 @@ public class Solution {
         }
     }
 }
+
+    //like BFS, but not exactly    bottom-up
+    public int uniquePaths(int m, int n) {
+        int[][] path = new int[m][n];
+        if(m < 1 || n < 1) return 0;
+
+        for(int i = 0; i <= m-1; i++) {
+            path[i][n-1] = 1;
+        }
+        for(int j = 0; j <= n-1; j++) {
+            path[m-1][j] = 1;
+        }
+        if(m > 1 && n > 1) {
+            for(int i = m-2; i >= 0; i--) {
+                for(int j = n-2; j >= 0; j--) {
+                    path[i][j] = path[i+1][j] + path[i][j+1];
+                }
+            }
+        }
+        return path[0][0];
+    }
