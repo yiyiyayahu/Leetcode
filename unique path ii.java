69e79d7159245d827241bdb8403285df4d0eaa45
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-22 14:00:36 -0400

    Update unique path ii.java

--- unique path ii.java
+++ unique path ii.java
@@ -39,3 +39,26 @@ public class Solution {
         return path[0][0];
     }
 }
+
+//better solution using dp
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] path = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(obstacleGrid[i][j] == 0) {
+                    if(i == 0 && j == 0) {
+                        path[i][j] = 1;
+                    } else {
+                        path[i][j] = (i>0?path[i-1][j]:0) + (j>0?path[i][j-1]:0); //Be careful
+                    }
+                }
+            }
+        }
+        return path[m-1][n-1];  
+    }
+
+}
