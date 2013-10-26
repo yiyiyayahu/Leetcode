d81f050e338adedeea433cbee48f42adf75c5f58
yiyiyayahu <yuanjie_qu@hotmail.com>
2013-10-26 18:19:32 -0400

    Update merge k sorted lists.java

--- merge k sorted lists.java
+++ merge k sorted lists.java
@@ -1,4 +1,3 @@
-
 public class Solution {
     public ListNode mergeKLists(ArrayList<ListNode> lists) {
         if(lists == null || lists.size() == 0) return null;
@@ -27,5 +26,36 @@ public class Solution {
         if(l1 != null) head.next = l1;
         else head.next = l2;
         return ret.next;
+    }
+}
+
+//use a heap to help sort the linked lists
+public class Solution {
+    public ListNode mergeKLists(ArrayList<ListNode> lists) {
+        if(lists == null || lists.size() == 0) return null;
+
+        PriorityQueue<ListNode> heap = new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>() {
+                public int compare(ListNode n1, ListNode n2) {
+                    if(n1.val < n2.val) return -1;
+                    else if(n1.val == n2.val) return 0;
+                    else return 1;
+                }
+            });
+        for(ListNode node:lists) {
+            if(node != null) heap.add(node);
+        }
+        ListNode head = null;
+        ListNode curr = null;
+        while(!heap.isEmpty()) {
+            if(head == null) {
+                head = heap.poll();
+                curr = head;
+            } else {
+                curr.next = heap.poll();
+                curr = curr.next;
+            }
+            if(curr.next != null) heap.add(curr.next);
+        }
+        return head;
     }
 }
